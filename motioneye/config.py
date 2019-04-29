29c162a033671c0afe885e9ddd167967456501ce
alexie <ualex73@gmail.com>
2019-04-29 08:17:12 +0200

    motionEye 0.40

--- motioneye/config.py
+++ motioneye/config.py
@@ -1380,7 +1380,7 @@ def motion_camera_dict_to_ui(data):
     ui['email_notifications_picture_time_span'] = 0
     command_notifications = []
     for e in on_event_start:
-        if e.count('sendmail'):
+        if e.count(' sendmail '):
             e = shlex.split(e)
 
             if len(e) < 10:
@@ -1404,7 +1404,7 @@ def motion_camera_dict_to_ui(data):
             except:
                 ui['email_notifications_picture_time_span'] = 0
 
-        elif e.count('webhook'):
+        elif e.count(' webhook '):
             e = shlex.split(e)
 
             if len(e) < 3:
@@ -1448,7 +1448,7 @@ def motion_camera_dict_to_ui(data):
 
     command_storage = []
     for e in on_movie_end:
-        if e.count('webhook'):
+        if e.count(' webhook '):
             e = shlex.split(e)
 
             if len(e) < 3:
